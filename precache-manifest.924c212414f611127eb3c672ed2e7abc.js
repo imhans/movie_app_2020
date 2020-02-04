5aff42904649b5e0ad609647b5b621977bd51ff7
imhans <imhans@temple.edu>
2020-02-04 00:24:49 -0500

    Updates

---
+++ precache-manifest.924c212414f611127eb3c672ed2e7abc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdeac26885f165955fa4c5944bfbd402",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "608f05bbf33f4542bb38",
+    "url": "/movie_app_2020/static/css/main.e083b577.chunk.css"
+  },
+  {
+    "revision": "07f2d2c5569d28c9abab",
+    "url": "/movie_app_2020/static/js/2.c0126add.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app_2020/static/js/2.c0126add.chunk.js.LICENSE"
+  },
+  {
+    "revision": "608f05bbf33f4542bb38",
+    "url": "/movie_app_2020/static/js/main.2789c5b4.chunk.js"
+  },
+  {
+    "revision": "90b45c5c50d92ea532b6",
+    "url": "/movie_app_2020/static/js/runtime-main.2c9a6a91.js"
+  }
+]);
